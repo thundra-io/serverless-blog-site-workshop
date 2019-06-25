94c7d39dfd96209c05b6ac7da7a1bedfaa49cc60
Serkan ÖZAL <serkan@thundra.io>
2019-06-25 18:04:32 +0300

    Enabled CORS

--- common.js
+++ common.js
@@ -1,6 +1,9 @@
 module.exports.sendHttpResponse = (statusCode, message, callback) => {
     const response = {
         statusCode: statusCode,
+        headers: {
+            "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+        },
         body: JSON.stringify(message)
     };
     callback(null, response);
