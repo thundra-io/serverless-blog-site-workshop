16018da855d67b87a5585e49e472309acec10c1a
Hamit Burak Emre <hamitburakemre@gmail.com>
2020-09-24 10:37:16 +0300

    Update blogPostService.js

--- service/blogPostService.js
+++ service/blogPostService.js
@@ -13,13 +13,13 @@ const WHITE_SPACE_REGEX = new RegExp("\\s+");
 const BANNED_BLOG_POST_WORDS = new Set(['server']);
 const MAX_BLOG_POST_LENGTH = 10000;
 
-AWS.config.update({region: process.env.AWS_REGION});
+AWS.config.update({ region: process.env.AWS_REGION });
 
 var dynamodb = new AWS.DynamoDB(); // new AWS.DynamoDB.DocumentClient();
 var sqs = new AWS.SQS();
 var sns = new AWS.SNS();
 // Only 'us-east-1', 'us-west-2', 'eu-west-1', 'ap-northeast-1', 'ap-southeast-1' and 'ap-southeast-2' regions support SMS
-var snsForSMS = new AWS.SNS({region: 'us-west-2'});
+var snsForSMS = new AWS.SNS({ region: 'us-west-2' });
 
 var esClient = new es.Client({
     hosts: [BLOG_POST_ES_HOST_NAME + ':' + BLOG_POST_ES_HOST_PORT]
@@ -45,7 +45,7 @@ module.exports.getBlogPost = (id) => {
     const params = {
         TableName: BLOG_POST_TABLE_NAME,
         Key: {
-            'id': {S: id}
+            'id': { S: id }
         }
     };
     return dynamodb.getItem(params).promise();
@@ -54,30 +54,30 @@ module.exports.getBlogPost = (id) => {
 module.exports.updateBlogPost = (id, post, state, prevState) => {
     console.log('Updating blog post with id: ' + id);
 
-    const attributeUpdates = { };
+    const attributeUpdates = {};
     if (post) {
         attributeUpdates['post'] = {
             Action: 'PUT',
-            Value: {S: post}
+            Value: { S: post }
         }
     }
     if (state) {
         attributeUpdates['state'] = {
             Action: 'PUT',
-            Value: {S: state}
+            Value: { S: state }
         }
     }
 
     const params = {
         TableName: BLOG_POST_TABLE_NAME,
         Key: {
-            'id': {S: id}
+            'id': { S: id }
         },
         AttributeUpdates: attributeUpdates,
         Expected: {
             'state': {
                 ComparisonOperator: "EQ",
-                Value: {S: prevState}
+                Value: { S: prevState }
             }
         },
         ReturnValues: 'ALL_NEW'
@@ -91,16 +91,16 @@ module.exports.saveBlogPost = (blogPost) => {
     const params = {
         TableName: BLOG_POST_TABLE_NAME,
         Item: {
-            'id': {S: blogPost.id},
-            'title': {S: blogPost.title},
-            'post': {S: blogPost.post},
-            'username': {S: blogPost.username},
-            'timestamp': {N: blogPost.timestamp.toString()},
-            'state': {S: blogPost.state}
+            'id': { S: blogPost.id },
+            'title': { S: blogPost.title },
+            'post': { S: blogPost.post },
+            'username': { S: blogPost.username },
+            'timestamp': { N: blogPost.timestamp.toString() },
+            'state': { S: blogPost.state }
         }
     };
     if (blogPost.phoneNumber && blogPost.phoneNumber.length > 0) {
-        params.Item['phoneNumber'] = {S: blogPost.phoneNumber};
+        params.Item['phoneNumber'] = { S: blogPost.phoneNumber };
     }
     return dynamodb.putItem(params).promise();
 };
@@ -111,7 +111,7 @@ module.exports.deleteBlogPost = (blogPostId) => {
     const params = {
         TableName: BLOG_POST_TABLE_NAME,
         Key: {
-            'id': {S: blogPostId}
+            'id': { S: blogPostId }
         },
         ReturnValues: 'ALL_OLD'
     };
@@ -149,7 +149,7 @@ module.exports.publishBlogPostNotification = (message, phoneNumber) => {
                         console.log('Couldn\'t send SMS as the phone number is invalid: ' + phoneNumber);
                         return resolve(err);
                     } else {
-                        return reject(err);
+                        return resolve(err);
                     }
                 });
         });
@@ -159,7 +159,10 @@ module.exports.publishBlogPostNotification = (message, phoneNumber) => {
         TopicArn: BLOG_POST_NOTIFICATION_TOPIC_ARN,
         Message: message
     };
-    const promise = sns.publish(params).promise();
+    const promise = new Promise((resolve, reject) => {
+        sns.publish(params).promise().then(resolve).catch(resolve);
+    });
+
     promises.push(promise);
     return Promise.all(promises);
 };
@@ -168,17 +171,17 @@ module.exports.saveBlogPostToIndex = (blogPost) => {
     console.log('Indexing blog post: ' + JSON.stringify(blogPost));
 
     return esClient.index({
-            index: 'blogpost-' + BLOG_POST_ES_INDEX_IDENTIFIER,
-            type: '_doc',
-            id: blogPost.id,
-            body: {
-                "title": blogPost.title,
-                "post": blogPost.post,
-                "username": blogPost.username,
-                "phoneNumber": blogPost.phoneNumber,
-                "timestamp": blogPost.timestamp,
-                "state": blogPost.state
-            }
+        index: 'blogpost-' + BLOG_POST_ES_INDEX_IDENTIFIER,
+        type: '_doc',
+        id: blogPost.id,
+        body: {
+            "title": blogPost.title,
+            "post": blogPost.post,
+            "username": blogPost.username,
+            "phoneNumber": blogPost.phoneNumber,
+            "timestamp": blogPost.timestamp,
+            "state": blogPost.state
+        }
     });
 };
 
@@ -186,15 +189,15 @@ module.exports.deleteBlogPostFromIndex = (blogPostId) => {
     console.log('Deleting blog post with id=' + blogPostId);
 
     return esClient.delete({
-            index: 'blogpost-' + BLOG_POST_ES_INDEX_IDENTIFIER,
-            type: '_doc',
-            id: blogPostId
+        index: 'blogpost-' + BLOG_POST_ES_INDEX_IDENTIFIER,
+        type: '_doc',
+        id: blogPostId
     });
 };
 
 module.exports.searchBlogPosts = (keyword, username, startTimestamp, endTimestamp, state) => {
     console.log('Searching blog posts for keyword=' + keyword + ', username=' + username +
-                ', startTimestamp=' + startTimestamp + ', endTimestamp=' + endTimestamp);
+        ', startTimestamp=' + startTimestamp + ', endTimestamp=' + endTimestamp);
 
     const conditions = [];
     if (keyword) {
